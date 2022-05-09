bbf56db5990d3c752216be0d35016f8894d1c5c4
gustavobarsan <gustavobarsan@gmail.com>
2022-05-09 15:16:46 -0300

    chore: find_professors and create_professor

--- src/routers/professor_router.py
+++ src/routers/professor_router.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter
 from models.professor_model import Professor
+from config.db import get_database
+from services.professor_services.find_professors import get_all_professors
 
+conn_db = get_database()
 professor = APIRouter()
-
-@professor.get('/professor')
-def find_professor():
-    return "TAES-2022.1"
 
 @professor.post('/professor')
 def create_professor(professor: Professor):
-    return "TAES-2022.2"
+    new_professor = dict(professor)
+    print(new_professor)
+    resp = conn_db.professor.insert_one(new_professor).inserted_id
+    return str(resp)
+
+@professor.get('/professor')
+def find_all_professors():
+    resp = get_all_professors()
+    return resp
 
 @professor.get('/professor/{id}')
-def create_professor():
+def find_professor():
     return "TAES-2022.1"
 
 @professor.put('/professor/{id}')
