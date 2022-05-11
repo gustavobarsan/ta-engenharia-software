2a60a2cff0e3efb2acdb96ace36acc6b39d9be11
FellipeGit <thenuuck@gmail.com>
2022-05-10 21:41:17 -0300

    Rota criar-professor criada

--- src/routers/professor_router.py
+++ src/routers/professor_router.py
@@ -2,17 +2,15 @@ from fastapi import APIRouter
 from models.professor_model import Professor
 from config.db import get_database
 from services.professor_services.find_professors import get_all_professors
+from services.professor_services.criar_professor import criar_professor
 
 conn_db = get_database()
 professor = APIRouter()
 
 @professor.post('/professor')
 def create_professor(professor: Professor):
-    new_professor = dict(professor)
-    print(new_professor)
-    resp = conn_db.professor.insert_one(new_professor).inserted_id
-    return str(resp)
-
+    return criar_professor(professor)
+    
 @professor.get('/professor')
 def find_all_professors():
     resp = get_all_professors()
